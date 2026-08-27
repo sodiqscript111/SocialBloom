1f802f3cde25d343d14dc3a41726e1e5e4a4d77b
sodiqscript111 <simidusodiqt@gmail.com>
2026-08-27 22:26:39 +0100

    Fix python path in user-services pytest config

--- services/user-services/tests/conftest.py
+++ services/user-services/tests/conftest.py
@@ -1,4 +1,9 @@
 import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
